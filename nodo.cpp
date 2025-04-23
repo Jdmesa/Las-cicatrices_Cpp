4db7329e83048e29ab5897a7621b862651263c8b
Julian Andres <julianandresdev@gmail.com>
2025-04-23 12:39:10 -0500

    archivos principales para creacion de mapa

---
+++ nodo.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Julian on 23/04/2025.
+//
+
+#include "nodo.h"
---
+++ nodo.h
@@ -0,0 +1,40 @@
+//
+// Created by Julian on 23/04/2025.
+//
+
+#ifndef NODO_H
+#define NODO_H
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Criatura;
+
+class nodo {
+    int fila;
+    int columna;
+    string tipo;
+    vector<Criatura*> criaturas;
+
+    public:
+    nodo(int fila, int columna, const string& tipo);
+    ~nodo();
+
+    int getFila() const;
+    int getColumna() const;
+    string getTipo() const;
+
+    void setTipo(const string& tipo);
+
+    void agregarCriatura(Criatura* criaturas);
+    void eliminarCriatura(Criatura* criatura);
+
+    const vector<Criatura*>& getCriaturas() const;
+
+
+};
+
+
+
+#endif //NODO_H
