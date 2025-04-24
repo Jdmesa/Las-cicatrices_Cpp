314967f18cfca08dd7d6dfaf5b4453a9d639362a
Julian Andres <julianandresdev@gmail.com>
2025-04-24 15:48:37 -0500

    Solucionado problema en nodo.cpp y getters anadidos, mas otras cosas

--- nodo.cpp
+++ nodo.cpp
@@ -3,3 +3,34 @@
 //
 
 #include "nodo.h"
+#include <algorithm>
+
+nodo::nodo(int fila, int columna, const string &tipo) : fila(fila), columna(columna), tipo(tipo) {}
+
+int nodo::getFila() const {
+    return fila;
+}
+
+int nodo::getColumna() const {
+    return columna;
+}
+
+string nodo::getTipo() const {
+    return tipo;
+}
+
+void nodo::setTipo(const string &tipoNuevo) {
+    tipo = tipoNuevo;
+}
+
+void nodo::agregarCriatura(Criatura *c) {
+    criaturas.push_back(c);
+}
+
+void nodo::eliminarCriatura(Criatura *c) {
+    criaturas.erase(remove(criaturas.begin(), criaturas.end(), c), criaturas.end());
+}
+
+const vector<Criatura *> & nodo::getCriaturas() const {
+    return criaturas;
+}
