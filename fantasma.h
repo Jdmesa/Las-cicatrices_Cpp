b94a31c2d8a7c5e78b3d6ba00f65f24c8ad20f39
Julian Andres <julianandresdev@gmail.com>
2025-05-03 12:53:45 -0500

    Nuevas clases, criaturas y atributos para las criaturas

---
+++ fantasma.h
@@ -0,0 +1,20 @@
+//
+// Created by Julian on 3/05/2025.
+//
+
+#ifndef FANTASMA_H
+#define FANTASMA_H
+
+
+
+class fantasma {
+private:
+    bool invisible;
+    public:
+    fantasma(bool inv);
+    bool getInv() const;
+};
+
+
+
+#endif //FANTASMA_H
---
+++ volador.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Julian on 3/05/2025.
+//
+
+#include "volador.h"
+
+bool volador::getVuela() const {
+    return volar;
+}
