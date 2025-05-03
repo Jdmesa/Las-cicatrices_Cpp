3be87a6274055a65d4e8c15ea4e55ac723d3e099
Julian Andres <julianandresdev@gmail.com>
2025-05-03 13:15:40 -0500

    organizado por modulos

--- Atributos/criatura.cpp
+++ Atributos/criatura.cpp
@@ -2,7 +2,7 @@
 // Created by Julian on 30/04/2025.
 //
 
-#include "criatura.h"
+#include "../criatura.h"
 // de momento no se necesita nada.
 criatura::criatura(int& fila, int& columna) : cicloEv(fila, columna) {
     cicloEv = "crio";
