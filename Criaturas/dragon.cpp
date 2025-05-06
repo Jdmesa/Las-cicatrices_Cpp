c9423dfcb9338f44775efdbc6f02ae919a666081
Jose <jdmesavelasquez10@gmail.com>
2025-05-06 18:07:44 -0500

    Función evolución en dragon, hecha

--- Criaturas/dragon.cpp
+++ Criaturas/dragon.cpp
@@ -1,7 +1,8 @@
 //
 // Created by Julian on 3/05/2025.
 //
-
+#include <random>
+#include <algorithm> // Para min y max
 #include "dragon.h"
 
 dragon::dragon(const string& nombre, int& fila, int& columna): nombre(nombre), criatura(fila, columna, 5) {}
@@ -30,9 +31,30 @@ void dragon::moverse(mapa &m) {
 
 }
 
-void dragon::evolucion(mapa &m) {
-    //TODO: implementar evolucion.
+void dragon::evolucion(mapa &m){
+    int numeroAdivinador = 5;
+    random_device rd2;
+    mt19937 gen2(rd2());
+    uniform_int_distribution<> distribucionRango(0, 6); // Rango de 0 a 6
+
+    int numeroEnRangoRandom = distribucionRango(gen2);
+    if (numeroEnRangoRandom == numeroAdivinador) {
+        if (cicloEv == "zombie") {
+            morir(m);
+        } else {
+            random_device rd3;
+            mt19937 gen3(rd3());
+            uniform_int_distribution<> distribucionOpciones(0, 2);
+            vector<string> opciones2 = { "adulto", "evolutivo", "zombie" };
+            int indice = distribucionOpciones(gen3);
+            string opcion = opciones2[indice];
+            setCiclo(opcion);
+
+            cout << "El dragon " << nombre << " evolucionó a " << opcion << endl;
+        }
+    }
 }
+
 
 void dragon::morir(mapa &m){
     vive = false;
